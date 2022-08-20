2bfe03363a3a641a38c1459798bef02772ba2634
Andres Vasquez <you@example.com>
2022-08-19 21:59:10 -0400

    clase 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,31 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  private nombre: string;
-  public apellido: string;
-  edad: number;
-  frutas: string[];
-
-  constructor() {
-    this.nombre = 'pepe';
-    this.apellido = 'perez';
-    this.edad = 10;
-    this.frutas = [
-      'pera',
-      'naranja',
-      'manzana',
-      'frutilla',
-      'mango',
-      'guineo',
-      'tomate',
-    ];
-  }
-
-  private restar(a: number, b: number): number {
-    return a - b;
-  }
-
-  public sumar(a: number, b: number): number {
-    return a + b;
-  }
+  constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContacComponent } from './pages/contac/contac.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HomeComponent, ContacComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
